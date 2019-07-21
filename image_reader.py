48bda2cfdbe603951b9eef7aac55e89e6c0013cc
keenan <keenan.ker51@gmail.com>
2019-07-20 20:43:36 -0400

    major updates to the visual processing

--- image_reader.py
+++ image_reader.py
@@ -1,4 +1,75 @@
-from PIL import Image
-from pytesseract import image_to_string
+# all image proecessing will happen here.
 
-print(image_to_string(Image.open('just_name.png')))
+from PIL import Image, ImageGrab
+from pytesseract import image_to_string
+import numpy as np
+from numpy import matrix
+
+
+#print(image_to_string(Image.open('just_name.png')))
+
+def get_text(img):
+    print(img)
+    print(image_to_string(Image.open(img)).split())
+    print("\n")
+    #return image_to_string(Image.open(img))
+
+def get_poke_name():
+    im = ImageGrab.grab(bbox=(287,138,575,169))
+    im.save('just_name.png')
+    #get_text(im)
+    #im = ImageGrab.grab()
+    #im.show()
+
+def outside_battle(state):
+    im = ImageGrab.grab(bbox=(58,536,266,613))
+    im.save('outside_{}.png'.format(state))
+
+def get_eff():
+    first = ImageGrab.grab(bbox=(304,704,407,719))
+    first.save('moves/frst.png')
+    scnd = ImageGrab.grab(bbox=(304,759,407,773))
+    scnd.save('moves/scnd.png')
+    thrd = ImageGrab.grab(bbox=(517,704,620,719))
+    thrd.save('moves/thrd.png')
+    fourth = ImageGrab.grab(bbox=(517,759,620,773))
+    fourth.save('moves/fourth.png')
+
+
+
+# sourced from https://gist.github.com/olooney/1246268
+def average_image_color(filename):
+	i = Image.open(filename)
+	h = i.histogram()
+
+	# split into red, green, blue
+	r = h[0:256]
+	g = h[256:256*2]
+	b = h[256*2: 256*3]
+
+	# perform the weighted average of each channel:
+	# the *index* is the channel value, and the *value* is its weight
+	return (
+		sum( i*w for i, w in enumerate(r) ) / sum(r),
+		sum( i*w for i, w in enumerate(g) ) / sum(g),
+		sum( i*w for i, w in enumerate(b) ) / sum(b)
+	)
+
+def return_avgs():
+    l = matrix(average_image_color('outside_start.png'))
+    d = matrix(average_image_color('outside_now.png'))
+    return np.mean(l-d)
+#get_poke_name()
+#outside_battle()
+#get_text('Tentacruel.png')
+
+#get_eff()
+first = ImageGrab.grab(bbox=(304,704,407,719))
+first.save('moves/frst.png')
+print(np.mean( matrix((average_image_color("moves/frst.png")))))
+print(average_image_color('moves/frst.png'))
+# print(np.mean( matrix((average_image_color('moves/scnd.png')))))
+
+print(np.mean( matrix((average_image_color('moves/thrd.png')))))
+print(average_image_color('moves/thrd.png'))
+# print(np.mean( matrix((average_image_color('moves/fourth.png')))))
--- trainer.py
+++ trainer.py
@@ -3,9 +3,16 @@
 from pynput.keyboard import Key, Controller
 import time
 from random import randint
+from image_reader import *
+from numpy import matrix
+import numpy as np
+
 
 bot = Controller()
 
+possible_pokemon = ("Pelipper", "Tentacruel", "Wingull", "Tentacool")
+sup_eff = np.mean( matrix((89.74063800277392, 83.78918169209432, 69.36407766990291)))
+print(sup_eff)
 def side_move():
     bot.press(Key.right)
     time.sleep(0.3)
@@ -24,12 +31,73 @@ def vert_move():
 time.sleep(3)
 
 def attack_mode():
-    for x in range(0,75):
-        print("                                  ",end='\r')
-        print("Spamming first attack: ",x,"/75", end='\r')
+    gone_yet = False # set to True if you want to just spam, with no checking for sup_eff
+    while True:
         bot.press('z')
         bot.release('z')
-        time.sleep(0.2)
+        print("                                     ",end='\r')
+        print("Attacking", end='\r')
+        if gone_yet == False:
+            print("                                     ",end='\r')
+            print("grabbing effectivness", end='\r')
+            time.sleep(1)
+            get_eff()
+            print(np.mean( matrix((average_image_color('moves/scnd.png')))))
+            if sup_eff- 0.5 <= np.mean( matrix((average_image_color("moves/frst.png")))) <= sup_eff+0.5:
+                bot.press('z')
+                bot.release('z')
+                print("                                     ",end='\r')
+                print("first move", end='\r')
+                gone_yet = True
+
+            elif sup_eff- 0.5 <= np.mean( matrix((average_image_color('moves/scnd.png'))))<= sup_eff+0.5:
+                bot.press(Key.down)
+                bot.release(Key.down)
+                bot.press('z')
+                bot.release('z')
+                print("                                     ",end='\r')
+                print("second move", end='\r')
+                gone_yet = True
+
+            elif sup_eff- 0.5 <= np.mean( matrix((average_image_color('moves/thrd.png'))))<= sup_eff+0.5:
+                print("thrd mve: {}".format(np.mean( matrix((average_image_color('moves/scnd.png'))))))
+                bot.press(Key.right)
+                bot.release(Key.right)
+                bot.press('z')
+                bot.release('z')
+                gone_yet = True
+
+            elif sup_eff- 0.5 <= np.mean( matrix((average_image_color('moves/fourth.png'))))<= sup_eff+0.5:
+                bot.press(Key.down)
+                bot.release(Key.down)
+                bot.press(Key.right)
+                bot.release(Key.right)
+                bot.press('z')
+                bot.release('z')
+                gone_yet = True
+            else:
+                bot.press('z')
+                bot.release('z')
+                print("                                     ",end='\r')
+                print("default move", end='\r')
+                gone_yet = True
+        else:
+            bot.press('z')
+            bot.release('z')
+
+        time.sleep(12)
+        outside_battle("now")
+        if return_avgs() <50:
+            break
+
+
+    ### DEPRIECIATED ###
+    # for x in range(0,75):
+    #     print("                                  ",end='\r')
+    #     print("Spamming first attack: ",x,"/75", end='\r')
+    #     bot.press('z')
+    #     bot.release('z')
+    #     time.sleep(0.2)
 
 def catch_mode():
     print("                                  ",end='\r')
@@ -49,10 +117,12 @@ def catch_mode():
 while True:
     print("                                      ",end='\r')
     print("Moving PC",end='\r')
-
-    for x in range(0,10):
+    outside_battle("start")
+    for x in range(0,5):
         side_move()
-    print("                                     ",end='\r')
-    print("Waiting for battle", end='\r')
-    time.sleep(7)
-    catch_mode()
+    outside_battle("now")
+    if return_avgs() >50:
+        print("                                     ",end='\r')
+        print("Waiting for battle", end='\r')
+        time.sleep(10)
+        attack_mode()
